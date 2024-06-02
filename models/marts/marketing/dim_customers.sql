5912d07267dc1b86b8c477386672b8ad1f30cf45
muuuuwa@gmail.com <muuuuwa@gmail.com>
2024-06-02 08:00:41 +0000

    ready for creating docs.

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -12,16 +12,25 @@ orders as (
 
 ),
 
+payment as (
+
+    select * 
+    from {{ ref('stg_stripe__payments') }}
+
+),
+
 customer_orders as (
 
     select
         customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders,
+        sum(payment.amount) as lifetime_value
 
     from orders
+    join payment on orders.order_id = payment.order_id
 
     group by 1
 
@@ -35,6 +44,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
